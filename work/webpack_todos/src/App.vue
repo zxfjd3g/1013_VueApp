bcd24f0133875e5257febbcc15da7edb59e7bde6
zxfjd3g <258147149@qq.com>
2017-02-17 14:33:51 +0800

    update

---
+++ work/webpack_todos/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <todo-header :add-todo="addTodo"></todo-header>
+      <todo-main :todos="todos"></todo-main>
+      <todo-footer></todo-footer>
+    </div>
+  </div>
+</template>
+<script>
+  import TodoHeader from './components/TodoHeader'
+  import TodoMain from './components/TodoMain'
+  import TodoFooter from './components/TodoFooter'
+
+  export default {
+    data () {
+      return {
+        todos: [
+          {text: '工作', isDone: true},
+          {text: '买房', isDone: false}
+        ]
+      }
+    },
+    methods: {
+      addTodo (todo) {
+        this.todos.unshift(todo)
+      }
+    },
+    components: {TodoHeader, TodoMain, TodoFooter}
+  }
+</script>
+<style>
+  /*app*/
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
