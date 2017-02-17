d2fcc5374fe92379203848a7d1620e06a8ae8ec8
zxfjd3g <258147149@qq.com>
2017-02-17 10:28:47 +0800

    VueApp

---
+++ work/webpack_demo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <hello></hello>
+  <img src="./assets/logo.png">
+  <p>app组件文本内容....</p>
+</template>
+<script>
+  import Hello from './components/Hello'
+
+  export default {
+    components: {Hello}
+  }
+</script>
+<style>
+  p {
+    background: red;
+    color: green;
+  }
+</style>
---
+++ work/webpack_demo/src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        title: 'Hello component title'
+      }
+    }
+  }
+</script>
+<style>
+  h1 {
+    background: green;
+    color: red;
+  }
+</style>
