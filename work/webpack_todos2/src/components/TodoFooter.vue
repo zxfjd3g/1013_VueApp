995521e2443332f4010d672ba85c3d0d6fef57c3
zxfjd3g <258147149@qq.com>
2017-02-20 16:06:06 +0800

    update

---
+++ work/webpack_todos2/src/components/TodoFooter.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="todo-footer">
+    <label>
+      <input type="checkbox" v-model="allDone"/>
+    </label>
+    <span>
+          <span>已完成{{doneCount}}</span> / 全部{{totalCount}}
+        </span>
+    <button class="btn btn-danger" v-show="doneCount>0" @click="deleteAllDone">清除已完成任务</button>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: ['totalCount', 'doneCount', 'deleteAllDone', 'updateAllDone'],
+    computed: {
+      allDone: {//双向数据绑定效果
+        get() {
+          return this.totalCount===this.doneCount && this.totalCount>0
+        },
+        set(value) {
+          this.updateAllDone(value)
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  /*footer*/
+  .todo-footer {
+    height: 40px;
+    line-height: 40px;
+    padding-left: 6px;
+    margin-top: 5px;
+  }
+
+  .todo-footer label {
+    display: inline-block;
+    margin-right: 20px;
+    cursor: pointer;
+  }
+
+  .todo-footer label input {
+    position: relative;
+    top: -1px;
+    vertical-align: middle;
+    margin-right: 5px;
+  }
+
+  .todo-footer button {
+    float: right;
+    margin-top: 5px;
+  }
+</style>
---
+++ work/webpack_todos2/src/components/TodoHeader.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="todo-header">
+    <input type="text" placeholder="请输入你的任务名称，按回车键确认"  v-model="inputText" @keyup.enter="add"/>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: ['addTodo'],
+    data () {
+      return {
+        inputText: null
+      }
+    },
+    methods: {
+      add () {
+        let inputText = this.inputText && this.inputText.trim()
+        if (inputText) {
+          // 创建todo对象
+          const todo = {text:inputText, isDone: false}
+          // 添加
+          this.addTodo(todo)
+          //清理
+          this.inputText = null
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  /*header*/
+  .todo-header input {
+    width: 560px;
+    height: 28px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 4px 7px;
+  }
+
+  .todo-header input:focus {
+    outline: none;
+    border-color: rgba(82, 168, 236, 0.8);
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+  }
+</style>
